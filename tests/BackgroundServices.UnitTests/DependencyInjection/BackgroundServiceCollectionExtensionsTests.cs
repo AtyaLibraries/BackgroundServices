6be58b6aca6233be3c3d482831866ffc09abd0b9
aasulyan <arsen.asulyan.1991@gmail.com>
2026-07-15 23:06:29 +0400

    Add scoped periodic background service

--- tests/BackgroundServices.UnitTests/DependencyInjection/BackgroundServiceCollectionExtensionsTests.cs
+++ tests/BackgroundServices.UnitTests/DependencyInjection/BackgroundServiceCollectionExtensionsTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Atya.Hosting.BackgroundServices.UnitTests.DependencyInjection;
 
@@ -10,7 +12,7 @@ public sealed class BackgroundServiceCollectionExtensionsTests
     {
         IServiceCollection services = null!;
 
-        Action action = () => services.AddAtyaBackgroundService<TestHostedService>();
+        Action action = () => services.AddAtyaBackgroundService<TestPeriodicHostedService>();
 
         action.Should().Throw<ArgumentNullException>()
             .WithParameterName(nameof(services));
@@ -21,14 +23,15 @@ public sealed class BackgroundServiceCollectionExtensionsTests
     {
         var services = new ServiceCollection();
 
-        services.AddAtyaBackgroundService<TestHostedService>();
+        services.AddSingleton<ILogger<TestPeriodicHostedService>>(NullLogger<TestPeriodicHostedService>.Instance);
+        services.AddAtyaBackgroundService<TestPeriodicHostedService>();
         using ServiceProvider provider = services.BuildServiceProvider();
 
         provider.GetServices<IHostedService>()
             .Should()
             .ContainSingle()
             .Which.Should()
-            .BeOfType<TestHostedService>();
+            .BeOfType<TestPeriodicHostedService>();
     }
 
     [Fact]
@@ -36,7 +39,8 @@ public sealed class BackgroundServiceCollectionExtensionsTests
     {
         var services = new ServiceCollection();
 
-        services.AddAtyaBackgroundService<TestHostedService>(options => options.Interval = TimeSpan.FromSeconds(7));
+        services.AddSingleton<ILogger<TestPeriodicHostedService>>(NullLogger<TestPeriodicHostedService>.Instance);
+        services.AddAtyaBackgroundService<TestPeriodicHostedService>(options => options.Interval = TimeSpan.FromSeconds(7));
         using ServiceProvider provider = services.BuildServiceProvider();
 
         provider.GetRequiredService<Microsoft.Extensions.Options.IOptions<PeriodicBackgroundServiceOptions>>()
@@ -46,14 +50,61 @@ public sealed class BackgroundServiceCollectionExtensionsTests
             .Be(TimeSpan.FromSeconds(7));
     }
 
-    private sealed class TestHostedService : IHostedService
+    [Fact]
+    public void AddAtyaScopedBackgroundService_Registers_HostedService()
     {
-        public Task StartAsync(CancellationToken cancellationToken)
+        var services = new ServiceCollection();
+
+        services.AddSingleton<ILogger<TestScopedPeriodicHostedService>>(NullLogger<TestScopedPeriodicHostedService>.Instance);
+        services.AddAtyaScopedBackgroundService<TestScopedPeriodicHostedService>();
+        using ServiceProvider provider = services.BuildServiceProvider();
+
+        provider.GetServices<IHostedService>()
+            .Should()
+            .ContainSingle()
+            .Which.Should()
+            .BeOfType<TestScopedPeriodicHostedService>();
+    }
+
+    [Fact]
+    public void AddAtyaScopedBackgroundService_Applies_Options_Configuration()
+    {
+        var services = new ServiceCollection();
+
+        services.AddSingleton<ILogger<TestScopedPeriodicHostedService>>(NullLogger<TestScopedPeriodicHostedService>.Instance);
+        services.AddAtyaScopedBackgroundService<TestScopedPeriodicHostedService>(options => options.FailureDelay = TimeSpan.FromSeconds(9));
+        using ServiceProvider provider = services.BuildServiceProvider();
+
+        provider.GetRequiredService<Microsoft.Extensions.Options.IOptions<PeriodicBackgroundServiceOptions>>()
+            .Value
+            .FailureDelay
+            .Should()
+            .Be(TimeSpan.FromSeconds(9));
+    }
+
+    private sealed class TestPeriodicHostedService : PeriodicBackgroundService
+    {
+        public TestPeriodicHostedService(ILogger<TestPeriodicHostedService> logger)
+            : base(logger)
+        {
+        }
+
+        protected override Task ExecuteIterationAsync(CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
+    }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+    private sealed class TestScopedPeriodicHostedService : ScopedPeriodicBackgroundService
+    {
+        public TestScopedPeriodicHostedService(
+            ILogger<TestScopedPeriodicHostedService> logger,
+            IServiceScopeFactory scopeFactory)
+            : base(logger, scopeFactory)
+        {
+        }
+
+        protected override Task ExecuteIterationAsync(IServiceProvider scopedServices, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
